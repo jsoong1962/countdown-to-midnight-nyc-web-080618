d9408f3e81c64cba45ada5e0cee7a8f06134cbdc
Johnny Soong <johnnysoong@hotmail.com>
2018-07-30 22:37:15 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,13 @@
 #write your code here
 
-def countdown(int)
-  count = 0
-  while int >= count
-  puts "#{int} SECOND(S)!"
-  int -= 1
-  end
-  "HAPPY NEW YEAR!"
-end
+# def countdown(int)
+#   count = 0
+#   while int >= count
+#   puts "#{int} SECOND(S)!"
+#   int -= 1
+#   end
+#   "HAPPY NEW YEAR!"
+# end
 
 
 def countdown_with_sleep(int)
