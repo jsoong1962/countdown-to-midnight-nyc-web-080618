8a5b098317dcb027294c1c5851f091e6c8ddb4a0
Johnny Soong <johnnysoong@hotmail.com>
2018-07-30 22:16:15 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
 def countdown(int)
   count = 0
   while countdown_with_sleep(int) >= count
-  puts "#{int} SECOND(S)!"
+  puts "#{countdown_with_sleep(int)} SECOND(S)!
   countdown_with_sleep(int) -= 1
   end
   "HAPPY NEW YEAR!"
@@ -13,4 +13,7 @@ end
 def countdown_with_sleep(int)
   sleep(int)
 end
+
+
+countdown(10)
 
