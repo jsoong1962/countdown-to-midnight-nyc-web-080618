a40c27e8fc9e702396c3a4100a48cb4a23cf32c6
Johnny Soong <johnnysoong@hotmail.com>
2018-07-30 22:09:22 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,13 @@
 #write your code here
 
 def countdown(int)
-  counter = 0
-  while counter <= int 
-  puts "#{counter} SECOND(S)!"
-  counter += 1
+  while int >= 0
+  puts "#{int} SECOND(S)!"
+  counter -= 1
   end
   "HAPPY NEW YEAR!"
 end
 
-
+counter(10)
 
 
